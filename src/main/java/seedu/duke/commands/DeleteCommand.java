05c6a9bbbe853bf9ae8434f00afd72c2693f726c
kaceycsn <119840829+kaceycsn@users.noreply.github.com>
2023-03-12 18:01:33 +0800

    Add logger, add exception throwing
    AddCommand and DeleteCommand class: add logger
    
    DeleteCommand class: throw exception when user input entry id is out of range

--- src/main/java/seedu/duke/commands/DeleteCommand.java
+++ src/main/java/seedu/duke/commands/DeleteCommand.java
@@ -1,6 +1,10 @@
 package seedu.duke.commands;
 
 import seedu.duke.entrylog.EntryLog;
+import seedu.duke.exceptions.InvalidEntryIdException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Represents the delete feature in PocketPal. Users may specify
@@ -8,10 +12,15 @@ import seedu.duke.entrylog.EntryLog;
  * e.g., <code>/delete 10</code>
  */
 public class DeleteCommand extends Command{
-
+    private static final String MESSAGE_INVALID_ENTRY_ID = "Please specify a valid entry ID!";
+    private static Logger logger = Logger.getLogger(DeleteCommand.class.getName());
     private Integer entryId;
 
-    public DeleteCommand(Integer inputId){
+    public DeleteCommand(Integer inputId, EntryLog entries) throws InvalidEntryIdException {
+        if(inputId <= 0 || inputId > entries.getSize()){
+            logger.log(Level.WARNING, "Input entry ID is invalid");
+            throw new InvalidEntryIdException(MESSAGE_INVALID_ENTRY_ID);
+        }
         this.entryId = inputId - 1;
     }
 
